d9160b23842fbb154ee755b164b3412b95892073
jdriverab <jdriverab@hotmail.com>
2021-11-30 23:28:54 -0300

    se agrega clase 12

---
+++ src/typescript/ObjetosLiterales.tsx
@@ -0,0 +1,41 @@
+
+interface Persona {
+    nombre:string;
+    edad:number;
+    direccion: Direccion;
+}
+
+interface Direccion {
+    pais:string;
+    casaNo: number;
+
+}
+
+const ObjetosLiterales = () => {
+
+    const persona: Persona = {
+
+        nombre:"Fernando",
+        edad:35,
+        direccion:{
+            pais:"Canada",
+            casaNo: 615
+        }
+    }
+
+    return (
+        <div>
+
+            <h3>Objetos Literales</h3>
+            <code>
+                <pre>
+                    {JSON.stringify(persona, ["nombre"],2)}
+                </pre>
+            </code>
+            
+            
+        </div>
+    )
+}
+
+export default ObjetosLiterales
---
+++ src/typescript/TiposBasicos.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const TiposBasicos = () => {
+
+    
+
+    return (
+        <div>
+            <h3>
+                Tipos basicos
+            </h3>
+            
+        </div>
+    )
+}
+
+export default TiposBasicos
